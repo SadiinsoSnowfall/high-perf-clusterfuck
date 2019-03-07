SadiinsoSnowfall/high-perf-clusterfuck

commit dc0937f0a6c22a1298d3f5d0465b98acaa2f6cfb

update chameleon stack

inria/hiepacs.scm
@@ -4,7 +4,6 @@
 ;;; Copyright © 2017, 2019 Inria
 
 (define-module (inria hiepacs)
-  #:use-module (inria storm)
   #:use-module (guix)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
@@ -16,35 +15,41 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz))
+  #:use-module (inria storm)
+  #:use-module (inria storm-pm2)
+  #:use-module (guix utils)
+  #:use-module (srfi srfi-1))
 
 (define-public chameleon
   (package
     (name "chameleon")
-    (version "0.9.1")
+    (version "1f74f316e4481ba7fe28b5101c410229bc06a6a3")
+    (home-page "https://gitlab.inria.fr/solverstack/chameleon")
+    (synopsis "Dense linear algebra solver")
+    (description
+     "Chameleon is a dense linear algebra solver relying on sequential
+task-based algorithms where sub-tasks of the overall algorithms are submitted
+to a run-time system.  Such a system is a layer between the application and
+the hardware which handles the scheduling and the effective execution of
+tasks on the processing units.  A run-time system such as StarPU is able to
+manage automatically data transfers between not shared memory
+area (CPUs-GPUs, distributed nodes).")
+    (license license:cecill-c)
     (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "http://morse.gforge.inria.fr/chameleon/"
-                    version "/chameleon-" version ".tar.gz"))
+              (method git-fetch)
+              (uri (git-reference
+                    (url home-page)
+                    (commit version)
+
+                    ;; We need the submodule in 'cmake_modules/morse_cmake'.
+                    (recursive? #t)))
+              (file-name (string-append name "-" version "-checkout"))
               (sha256
                (base32
-                "0875bngwzxgycm44p7nndzsy9xcbc58rn08vq08041kzpnwhzk3x"))
-              (patches (search-patches "inria/patches/chameleon-lapacke.patch"))))
+                "1640xglaq4ah5jiv339p560fvxkkpjscf22bz9s0ligqjc0hyp4j"))))
     (build-system cmake-build-system)
     (arguments
-     '(#:configure-flags
-       (let ((blas (assoc-ref %build-inputs "blas"))
-             (lapack (assoc-ref %build-inputs "lapack")))
-         (list "-DMORSE_VERBOSE_FIND_PACKAGE=ON"
-               (string-append "-DBLAS_DIR=" blas)
-               (string-append "-DLAPACK_DIR=" blas)))
-
-       ;; Sometimes morse/precision_generator/Conversion.py would be called
-       ;; too early, leading to:
-       ;;
-       ;;    from subs import subs;
-       ;; EOFError: EOF read where object expected
-       #:parallel-build? #f
+     '(#:configure-flags '("-DBUILD_SHARED_LIBS=ON")
 
        ;; FIXME: Test too long for gitlab-runner CI
        #:tests? #f
@@ -56,25 +61,24 @@
                        ;; to be writable.
                        (setenv "HOME" (getcwd))
                        #t)))))
-    (inputs `(("starpu" ,starpu)
-              ("blas" ,openblas)
-              ("lapack" ,lapack)
-              ("mpi" ,openmpi)
-              ("hwloc" ,hwloc "lib")))
+    (propagated-inputs `(("starpu" ,starpu)
+                         ("lapack" ,openblas)))
     (native-inputs `(("pkg-config" ,pkg-config)
                      ("gfortran" ,gfortran)
-                     ("python" ,python-2)))
-    (home-page "https://project.inria.fr/chameleon/")
-    (synopsis "Dense linear algebra solver")
-    (description
-     "Chameleon is a dense linear algebra solver relying on sequential
-task-based algorithms where sub-tasks of the overall algorithms are submitted
-to a run-time system.  Such a system is a layer between the application and
-the hardware which handles the scheduling and the effective execution of
-tasks on the processing units.  A run-time system such as StarPU is able to
-manage automatically data transfers between not shared memory
-area (CPUs-GPUs, distributed nodes).")
-    (license license:cecill-c)))
+                     ("python" ,python-2)))))
+
+;; TODO: fix starpu+openmpi first
+;; (define-public chameleon+openmpi
+;;   (package
+;;     (inherit chameleon)
+;;     (name "chameleon-openmpi")
+;;     (propagated-inputs `(("mpi" ,openmpi)
+;;                          ("starpu" ,starpu+openmpi)
+;;                          ,@(delete `("starpu" ,starpu) (package-propagated-inputs chameleon))))
+;;     (arguments
+;;      (substitute-keyword-arguments (package-arguments chameleon)
+;;        ((#:configure-flags flags '())
+;;         `(cons "-DCHAMELEON_USE_MPI=ON" ,flags))))))
 
 (define-public maphys
   (package

inria/storm-pm2.scm
@@ -1,0 +1,212 @@
+;;; This module extends GNU Guix and is licensed under the same terms, those
+;;; of the GNU GPL version 3 or (at your option) any later version.
+;;;
+;;; Copyright © 2017, 2018 Inria
+
+(define-module (inria storm-pm2)
+  #:use-module (guix)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix svn-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages mpi)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages linux)
+  #:use-module (inria eztrace)
+  #:use-module (inria simgrid)
+  #:use-module (ice-9 match))
+
+(define %pm2-home-page "http://pm2.gforge.inria.fr/")
+(define %pm2-revision 27924)
+(define %padicotm-revision 5179)
+(define %pm2-svn "https://scm.gforge.inria.fr/anonscm/svn/pm2/trunk")
+(define %padicotm-svn "https://scm.gforge.inria.fr/anonscm/svn/padico/PadicoTM/trunk")
+(define %patch-path "inria/patches/autogen_building-tools.patch")
+
+(define-public puk
+  (package
+   (name "puk")
+   (version (number->string %padicotm-revision))
+   (home-page (string-append %pm2-home-page "/PadicoTM"))
+   (source (origin
+	    (method svn-fetch)
+	    (uri (svn-reference
+		  (url (string-append %padicotm-svn "/PadicoTM/Puk"))
+		  (revision %padicotm-revision)))
+	    (sha256
+	     (base32 "1cv20rqn210l59fq4h8i3plkinasfw3n433rsqr8gj509qfj24ag"))
+	    (patches (search-patches %patch-path))))
+   (build-system gnu-build-system)
+   (arguments
+    '(#:out-of-source? #t
+      #:configure-flags '("--enable-optimize"
+			  "--disable-debug"
+			  "--disable-trace")
+      #:phases (modify-phases %standard-phases
+	         (add-after 'unpack 'fix-hardcoded-paths
+		   (lambda _
+		     (substitute* "building-tools/common_vars.mk.in"
+		       (("/bin/sh")  (which "sh")))
+		     #t))
+		 (delete 'check))))
+   (native-inputs
+    `(("pkg-config" ,pkg-config)
+      ("autoconf", autoconf)
+      ("automake", automake)))
+   (propagated-inputs
+    `(("expat" ,expat)))
+   (synopsis "dummy")
+   (description "Dummy")
+   (license license:lgpl2.0)))
+
+(define-public pioman
+  (package
+   (name "pioman")
+   (version (number->string %pm2-revision))
+   (home-page (string-append %pm2-home-page "/pioman"))
+   (source (origin
+	    (method svn-fetch)
+	    (uri (svn-reference
+		  (url (string-append %pm2-svn "/pioman"))
+		  (revision %pm2-revision)))
+	    (sha256
+	     (base32 "1jhdxsg262h62pb3k0y4srz6ag4nfzh15y05ifc9wrcnhqzcyqnm"))
+	    (patches (search-patches %patch-path))))
+   (build-system gnu-build-system)
+   (arguments
+    '(#:out-of-source? #t
+      #:configure-flags '("--enable-optimize"
+			  "--disable-debug"
+			  "--with-pthread")
+      #:phases (modify-phases %standard-phases
+	         (add-after 'unpack 'fix-hardcoded-paths
+		   (lambda _
+		     (substitute* "building-tools/common_vars.mk.in"
+		       (("/bin/sh")  (which "sh")))
+		     #t))
+		 (delete 'check))))
+   (native-inputs
+    `(("pkg-config" ,pkg-config)
+      ("autoconf" ,autoconf)
+      ("automake" ,automake)))
+   (propagated-inputs
+    `(("puk" ,puk)
+      ("hwloc" ,hwloc "lib")))
+   (synopsis "dummy")
+   (description "Dummy")
+   (license license:lgpl2.0)))
+
+(define-public padicotm
+  (package
+   (name "padicotm")
+   (version (number->string %padicotm-revision))
+   (home-page (string-append %pm2-home-page "/PadicoTM"))
+   (source (origin
+	    (method svn-fetch)
+	    (uri (svn-reference
+		  (url (string-append %padicotm-svn "/PadicoTM/PadicoTM"))
+		  (revision %padicotm-revision)))
+	    (sha256
+	     (base32 "0g1wggawjzf843l1m2bgw01c7ljb6p95haby77fl26434w25sl6a"))
+	    (patches (search-patches %patch-path))))
+   (build-system gnu-build-system)
+   (arguments
+    '(#:out-of-source? #t
+      #:configure-flags '("--enable-optimize"
+			  "--disable-debug"
+			  "--with-pioman"
+			  "--without-pukabi")
+      #:phases (modify-phases %standard-phases
+	         (add-after 'unpack 'fix-hardcoded-paths
+		   (lambda _
+		     (substitute* "building-tools/common_vars.mk.in"
+		       (("/bin/sh")  (which "sh")))
+		     #t))
+		 (delete 'check))))
+   (native-inputs
+    `(("pkg-config" ,pkg-config)
+      ("autoconf" ,autoconf)
+      ("automake" ,automake)
+      ("hwloc" ,hwloc "lib")
+      ("rdma-core" ,rdma-core)
+      ("psm" ,psm)
+      ("psm2" ,psm2)))
+   (propagated-inputs
+    `(("puk" ,puk)
+      ("pioman" ,pioman)))
+   (synopsis "dummy")
+   (description "Dummy")
+   (license license:lgpl2.0)))
+
+(define-public padicotm-mini
+  (package
+   (inherit padicotm)
+   (name "padicotm-mini")
+   (arguments
+    (substitute-keyword-arguments (package-arguments padicotm)
+      ((#:configure-flags flags '())
+       `(cons "--without-pioman" (delete "--with-pioman" ,flags)))))
+   (propagated-inputs
+    `(("puk" ,puk)))))
+
+(define-public nmad
+  (package
+   (name "nmad")
+   (version (number->string %pm2-revision))
+   (home-page (string-append %pm2-home-page "/NewMadeleine"))
+   (source (origin
+	    (method svn-fetch)
+	    (uri (svn-reference
+		  (url (string-append %pm2-svn "/nmad"))
+		  (revision %pm2-revision)))
+	    (sha256
+	     (base32 "0kg4205y0k4ww1rzr97kkf1gfs4xwf1b1v317ypl0iv4y3rv604y"))
+	    (patches (search-patches %patch-path))))
+   (build-system gnu-build-system)
+   (arguments
+    '(#:out-of-source? #t
+      #:configure-flags '("--enable-optimize"
+			  "--disable-debug"
+			  "--with-pioman"
+			  "--without-pukabi"
+			  "--enable-mpi"
+			  "--disable-sampling")
+      #:phases (modify-phases %standard-phases
+		 (add-after 'unpack 'fix-hardcoded-paths
+		   (lambda _
+		     (substitute* "building-tools/common_vars.mk.in"
+		       (("/bin/sh")  (which "sh")))
+		     #t))
+		 (delete 'check))))
+   (native-inputs
+    `(("pkg-config" ,pkg-config)
+      ("autoconf" ,autoconf)
+      ("automake" ,automake)
+      ("gfortran" ,gfortran)
+      ("hwloc" ,hwloc "lib")))
+   (propagated-inputs
+    `(("padicotm" ,padicotm)))
+   (inputs
+    `(("rdma-core" ,rdma-core)
+      ("psm" ,psm)
+      ("psm2" ,psm2)))
+   (synopsis "dummy")
+   (description "Dummy")
+   (license license:lgpl2.0)))
+
+(define-public nmad-mini
+  (package
+   (inherit nmad)
+   (name "nmad-mini")
+   (arguments
+    (substitute-keyword-arguments (package-arguments nmad)
+      ((#:configure-flags flags '())
+       `(cons "--without-pioman" (delete "--with-pioman" ,flags)))))
+   (propagated-inputs
+    `(("padicotm" ,padicotm-mini)
+      ,@(delete `("padicotm" ,padicotm) (package-inputs nmad))))))

inria/storm.scm
@@ -17,6 +17,7 @@
   #:use-module (gnu packages gdb)
   #:use-module (gnu packages maths)
   #:use-module (gnu packages autotools)
+  #:use-module (inria storm-pm2)
   #:use-module (inria eztrace)
   #:use-module (inria simgrid)
   #:use-module (ice-9 match))
@@ -27,6 +28,9 @@
 (define (starpu-url version)
   (string-append %starpu-home-page "/files/starpu-"
                  version "/starpu-" version ".tar.gz"))
+
+(define %starpu-commit "1c35b01a4cdb79709adf275a48f5a4442bd1d875")
+(define %starpu-git "https://scm.gforge.inria.fr/anonscm/git/starpu/starpu.git")
 
 (define-public starpu-1.1
   (package
@@ -85,8 +89,8 @@
      `(("pkg-config" ,pkg-config)
        ("gdb" ,gdb)))                             ;used upon test failure
     (inputs `(("fftw" ,fftw)
-              ("fftwf" ,fftwf)
-              ("hwloc" ,hwloc "lib")))
+              ("fftwf" ,fftwf)))
+    (propagated-inputs  `(("hwloc" ,hwloc "lib")))
     (synopsis "Run-time system for heterogeneous computing")
     (description
      "StarPU is a run-time system that offers support for heterogeneous
@@ -97,44 +101,115 @@ implementing data coherency across the machine), but it also makes sure the
 kernels are executed as efficiently as possible.")
     (license lgpl2.1+)))
 
-(define-public starpu
+(define-public starpu-1.2
   (package
     (inherit starpu-1.1)
-    (version "1.2.5")
+    (version "1.2.6")
     (source (origin
               (method url-fetch)
-              (uri (starpu-url "1.2.5"))
+              (uri (starpu-url "1.2.6"))
               (sha256
                (base32
-                "1sfs5cnrda2nacgvmljbc91mrh1n9s7s3i90h0fawk7fr7jyl8hz"))))
+                "0k4n1n8zwdmw7idjzb4hyd1bhi9iyslsbb126xcfvdkpcxksfrzb"))))
     (native-inputs
      ;; Some tests require bc and Gnuplot.
      `(("bc" ,bc)
        ("gnuplot" ,gnuplot)
        ,@(package-native-inputs starpu-1.1)))))
 
-(define-public starpu+simgrid
-  (package
-    (inherit starpu)
-    (name "starpu-simgrid")
-    (inputs `(("simgrid" ,simgrid)
-              ,@(package-inputs starpu)))
-    (arguments
-     (substitute-keyword-arguments (package-arguments starpu)
-       ((#:configure-flags flags '())
-        `(cons "--enable-simgrid" ,flags))))))
 
-(define-public starpu+fxt
-  ;; When FxT support is enabled, performance is degraded, hence the separate
-  ;; package.
+(define-public starpu
   (package
-    (inherit starpu)
-    (name "starpu-fxt")
-    (inputs `(("fxt" ,fxt)
-              ,@(package-inputs starpu)))
-    (arguments
-     (substitute-keyword-arguments (package-arguments starpu)
-       ((#:configure-flags flags '())
-        `(cons "--with-fxt" ,flags))))))
+    (inherit starpu-1.2)
+    (version "1.3.0rc1")
+    (source (origin
+              (method url-fetch)
+              (uri "http://starpu.gforge.inria.fr/files/starpu-1.3.0rc1/starpu-1.3.0.tar.gz")
+              (sha256
+               (base32
+                "12c84p0f4krf0bmfmdcfngp90qcpfzc8z4cq0pv98m0gy7asgzhy"))))
+   (arguments
+    (substitute-keyword-arguments (package-arguments starpu-1.2)
+      ((#:phases phases '())
+       (append phases '((add-after 'patch-source-shebangs 'fix-hardcoded-paths
+                          (lambda _
+                            (substitute* "min-dgels/base/make.inc"
+                              (("/bin/sh")  (which "sh")))
+                            #t)))))))))
 
-;; TODO: Add variants with MPI support.
+;; TODO: build fails, implicit declaration of function ‘MPI_Type_hvector’
+;; (define-public starpu+openmpi
+;;   (package
+;;     (inherit starpu)
+;;     (name "starpu-openmpi")
+;;     (inputs `(("mpi" ,openmpi)
+;;               ,@(package-inputs starpu)))))
+
+;; TODO: build fails, fatal error: boost/intrusive_ptr.hpp: No such file or directory
+;; (define-public starpu+simgrid
+;;   (package
+;;     (inherit starpu)
+;;     (name "starpu-simgrid")
+;;     (inputs `(("simgrid" ,simgrid)
+;;               ,@(package-inputs starpu)))
+;;     (arguments
+;;      (substitute-keyword-arguments (package-arguments starpu)
+;;        ((#:configure-flags flags '())
+;;         `(cons "--enable-simgrid" ,flags))))))
+
+;; TODO: tests fail
+;; (define-public starpu+fxt
+;;   ;; When FxT support is enabled, performance is degraded, hence the separate
+;;   ;; package.
+;;   (package
+;;     (inherit starpu)
+;;     (name "starpu-fxt")
+;;     (inputs `(("fxt" ,fxt)
+;;               ,@(package-inputs starpu)))
+;;     (arguments
+;;      (substitute-keyword-arguments (package-arguments starpu)
+;;        ((#:configure-flags flags '())
+;;         `(cons "--with-fxt" ,flags))))))
+
+(define-public starpu-git
+  (package
+   (inherit starpu)
+   (name "starpu-git")
+   (version %starpu-commit)
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url %starpu-git)
+                  (commit %starpu-commit)))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32 "0iqwyr5hd8i3h365r5vrnzjjyb3pf6kx7gyr3cph2jzj1d63cp2f"))))
+   (native-inputs
+    `(("libtool" ,libtool)
+      ("autoconf" ,autoconf)
+      ("automake" ,automake)
+      ,@(package-native-inputs starpu)))))
+
+(define-public starpu+nmad
+  (package
+   (inherit starpu-git)
+   (name "starpu-nmad")
+   (version %starpu-commit)
+   (arguments
+    (substitute-keyword-arguments (package-arguments starpu-git)
+      ((#:configure-flags flags '())
+       `(cons "--enable-nmad" ,flags))))
+   (inputs
+    `(("nmad" ,nmad)
+      ,@(package-inputs starpu-git)))))
+
+(define-public starpu+madmpi
+  (package
+   (inherit starpu+nmad)
+   (name "starpu-madmpi")
+   (arguments
+    (substitute-keyword-arguments (package-arguments starpu+nmad)
+                                  ((#:configure-flags flags '())
+                                   `(delete "--enable-nmad" ,flags))))
+   (inputs `(("nmad" ,nmad-mini)
+             ,@(delete `("nmad" ,nmad) (package-inputs starpu+nmad))))))

commit 3bd4109fc717f85660800209aad6edfba7791db4

hiepacs: Fix typo.
Typo introduced in dc0937f0a6c22a1298d3f5d0465b98acaa2f6cfb.

inria/hiepacs.scm
@@ -14,7 +14,7 @@
   #:use-module (gnu packages mpi)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
-  #:use-module (gnu packages python-xyz))
+  #:use-module (gnu packages python-xyz)
   #:use-module (inria storm)
   #:use-module (inria storm-pm2)
   #:use-module (guix utils)
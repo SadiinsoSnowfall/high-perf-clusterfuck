SadiinsoSnowfall/high-perf-clusterfuck

commit 8181401225afd347907cbd98dbb4c1f2936e588b

starpu: Build 1.1 and 1.2 against 'openmpi-with-mpi1-compat'.
* inria/storm.scm (starpu-1.1)[propagated-inputs]: Replace OPENMPI with
OPENMPI-WITH-MPI1-COMPAT.
(starpu-1.3)[propagated-inputs]: New field.

inria/storm.scm
@@ -20,6 +20,7 @@
   #:use-module (gnu packages python)
   #:use-module (inria storm-pm2)
   #:use-module (inria eztrace)
+  #:use-module (inria mpi)
   #:use-module (inria simgrid)
   #:use-module (ice-9 match))
 
@@ -96,7 +97,7 @@
     (inputs `(("fftw" ,fftw)
               ("fftwf" ,fftwf)))
     (propagated-inputs  `(("hwloc" ,hwloc "lib")
-                          ("mpi" ,openmpi)))
+                          ("mpi" ,openmpi-with-mpi1-compat)))
     (synopsis "Run-time system for heterogeneous computing")
     (description
      "StarPU is a run-time system that offers support for heterogeneous
@@ -146,7 +147,10 @@ kernels are executed as efficiently as possible.")
                           (lambda _
                             (substitute* "min-dgels/base/make.inc"
                               (("/bin/sh")  (which "sh")))
-                            #t)))))))))
+                            #t)))))))
+   (propagated-inputs  `(("mpi" ,openmpi)
+                         ,@(alist-delete "mpi"
+                                         (package-native-inputs starpu-1.2))))))
 
 (define-public starpu
   starpu-1.3)

commit 6c618a4159e01fb5579ad1bd2c3d9ebf7ab3c199

starpu@1.3: Fix 'propagated-inputs'.
Fixes a regression introduced in
8181401225afd347907cbd98dbb4c1f2936e588b.

* inria/storm.scm (starpu-1.3)[propagated-inputs]: Fix typo.

inria/storm.scm
@@ -151,7 +151,7 @@ kernels are executed as efficiently as possible.")
                             #t)))))))
    (propagated-inputs  `(("mpi" ,openmpi)
                          ,@(alist-delete "mpi"
-                                         (package-native-inputs starpu-1.2))))))
+                                         (package-propagated-inputs starpu-1.2))))))
 
 (define-public starpu
   starpu-1.3)
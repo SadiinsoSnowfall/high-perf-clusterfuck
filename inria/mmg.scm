325ef475471f72ee95302a4b7da50a8c979d242a
Ludovic Courtès <ludovic.courtes@inria.fr>
2019-11-18 15:31:57 +0100

    mmg: Add missing copyright header.

--- inria/mmg.scm
+++ inria/mmg.scm
@@ -1,3 +1,8 @@
+;;; This module extends GNU Guix and is licensed under the same terms, those
+;;; of the GNU GPL version 3 or (at your option) any later version.
+;;;
+;;; Copyright © 2019 Inria
+
 (define-module (inria mmg)
   #:use-module (guix packages)
   #:use-module (guix download)

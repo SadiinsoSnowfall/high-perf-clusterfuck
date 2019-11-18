6cb6c96db7d8989990ea6af9eb56f67a3ccc25f1
Algiane <algiane.froehly@inria.fr>
2019-11-18 15:29:05 +0100

    Add mmg.

---
+++ inria/mmg.scm
@@ -0,0 +1,33 @@
+(define-module (inria mmg)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix licenses))
+
+(define-public mmg
+  (package
+    (name "mmg")
+    (version "5.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://github.com/MmgTools/mmg/archive/v"
+                           version ".tar.gz"))
+       (sha256
+        (base32
+         "0fpr0y1fy9wyda4678qzjlzravh3cns67m3b79b31bqqqjk851zb"))))
+    (build-system cmake-build-system)
+    (arguments
+     '(#:configure-flags '("-DBUILD_TESTING=ON"
+                           "-DONLY_VERY_SHORT_TESTS=ON")
+       #:tests? #t))
+    (home-page "https://www.mmgtools.org/")
+    (synopsis "Library and applications for simplicial mesh adaptation.")
+    (description
+     "Mmg gather open-source software for simplicial mesh modifications
+     (2D, 3D surfacic and 3D volumic). It allows: mesh quality improvement,
+      mesh adaptation on an isotropic or anisotropic sizemap,
+     isovalue discretization and lagrangian movement.")
+    (license lgpl3+)
+    )
+  )

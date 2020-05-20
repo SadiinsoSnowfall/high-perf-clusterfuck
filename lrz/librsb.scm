bb976949d8c36bc3a7780156bcef7841608c7497
Michele Martone <michelemartone@users.sourceforge.net>
2020-05-20 12:43:39 +0200

    Add librsb package (version 1.2.0.8)

---
+++ lrz/librsb.scm
@@ -0,0 +1,43 @@
+;;; This module extends GNU�| Guix and is licensed under the same terms, those
+;;; of the GNU GPL version 3 or (at your option) any later version.
+;;;
+;;; Copyright © 2017, 2019 Inria
+;;; Copyright © 2020 Michele MARTONE (LRZ)
+
+;;; This module has been developed under the PRACE-6IP-WP8 EU project LyNcs (GRANT AGREEMENT NUMBER 823767 -- PRACE-6IP).
+
+(define-module (lrz librsb)
+  #:use-module (guix)
+  #:use-module (guix build-system gnu)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages mpi) ;; for hwloc
+  )
+
+(define-public librsb
+  (package
+    (name "librsb")
+    (version "1.2.0.8")
+    (source (origin
+              (uri "http://download.sourceforge.net/librsb/librsb-1.2.0.8.tar.gz")
+              (method url-fetch)
+              (sha256
+               (base32
+                "1jw09lhaqcgm2mfmp5qbbi48lzmf0w7xvgza2gg0mn3632dd3swb"))
+              ))
+    (build-system gnu-build-system)
+    (inputs `(
+              ("gfortran" ,gfortran "lib")
+              ("gfortran" ,gfortran)
+              ("hwloc" ,hwloc)
+              ("zlib" ,zlib)
+              ))
+    (synopsis "librsb: A shared memory parallel sparse matrix computations library for the Recursive Sparse Blocks format")
+    (description
+     "librsb is a library for sparse matrix computations featuring the Recursive Sparse Blocks (RSB) matrix format. This format allows cache efficient and multi-threaded (that is, shared memory parallel) operations on large sparse matrices. The most common operations necessary to iterative solvers are available, e.g.: matrix-vector multiplication, triangular solution, rows/columns scaling, diagonal extraction / setting, blocks extraction, norm computation, formats conversion. The RSB format is especially well suited for symmetric and transposed multiplication variants. Most numerical kernels code is auto generated, and the supported numerical types can be chosen by the user at build time. librsb can also be built serially (without OpenMP parallelism), if required.
+librsb also implements the Sparse BLAS standard, as specified in the [BLAS Technical Forum] documents.
+")
+    (license license:lgpl3+)
+    (home-page "http://librsb.sourceforge.net/")))

8625942b7d6cc5ba340848e422d35e896918fee0
AGULLO Emmanuel <emmanuel.agullo@inria.fr>
2020-06-19 00:02:04 +0200

    New git repository for starpu

--- inria/storm.scm
+++ inria/storm.scm
@@ -35,7 +35,7 @@
   (string-append %starpu-home-page "/files/starpu-"
                  version "/starpu-" version ".tar.gz"))
 
-(define %starpu-git "https://scm.gforge.inria.fr/anonscm/git/starpu/starpu.git")
+(define %starpu-git "git@gitlab.inria.fr:starpu/starpu.git")
 
 (define-public starpu-1.1
   (package

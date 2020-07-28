a07c0cfdc6cc7ace6d41934a9ba3d1a1bc832cff
Nathalie Furmento <nathalie.furmento@labri.fr>
2020-07-28 16:02:29 +0200

    inria/storm.scm: update git address for StarPU

--- inria/storm.scm
+++ inria/storm.scm
@@ -35,8 +35,7 @@
   (string-append %starpu-home-page "/files/starpu-"
                  version "/starpu-" version ".tar.gz"))
 
-;; Waiting for full migration before pointing to: https://gitlab.inria.fr/starpu/starpu.git
-(define %starpu-git "https://scm.gforge.inria.fr/anonscm/git/starpu/starpu.git")
+(define %starpu-git "https://gitlab.inria.fr/starpu/starpu.git")
 
 (define-public starpu-1.1
   (package

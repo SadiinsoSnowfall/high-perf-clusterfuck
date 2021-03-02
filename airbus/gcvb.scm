SadiinsoSnowfall/high-perf-clusterfuck

commit 45cd7aeb495f230fb3735cf74e14584144ae6d28

Add missing licenses.

airbus/gcvb.scm
@@ -1,7 +1,7 @@
 ;;; This module extends GNU Guix and is licensed under the same terms, those
 ;;; of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2017, 2019, 2020 Inria
+;;; Copyright © 2017, 2019, 2020, 2021 Inria
 
 (define-module (airbus gcvb)
   #:use-module (guix)
@@ -92,7 +92,7 @@
       "A library for automatically generating command line interfaces.")
     (description
       "A library for automatically generating command line interfaces.")
-    (license #f)))
+    (license license:asl2.0)))
 
 (define-public python-dash-dangerously-set-inner-html
   (package
@@ -333,7 +333,7 @@
       "Bootstrap themed components for use in Plotly Dash")
     (description
       "Bootstrap themed components for use in Plotly Dash")
-    (license #f)))
+    (license license:asl2)))
 
 (define-public python-pyyaml-5
   (package

inria/hiepacs.scm
@@ -1,7 +1,7 @@
 ;;; This module extends GNU Guix and is licensed under the same terms, those
 ;;; of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2017, 2019 Inria
+;;; Copyright © 2017, 2019, 2021 Inria
 
 (define-module (inria hiepacs)
   #:use-module (guix)
@@ -450,7 +450,7 @@ such as: namespaces, templates, exceptions, etc.")
     ;;'(#:configure-flags '("-DBLASPP_BUILD_TESTS=ON")))
     (build-system cmake-build-system)
     (propagated-inputs `(("openblas" ,openblas))) ;; technically only blas
-    (license #f)))
+    (license license:bsd-3)))
 
 (define-public lapackpp
   (package
@@ -481,7 +481,7 @@ etc.")
     ;; tests would need testsweeper https://bitbucket.org/icl/testsweeper
     (build-system cmake-build-system)
     (inputs `(("blaspp" ,blaspp)))
-    (license #f)))
+    (license license:bsd-3)))
 
 (define-public pastix-6
   (package

ufrgs/ufrgs.scm
@@ -1,7 +1,7 @@
 ;;; This module extends GNU Guix and is licensed under the same terms, those
 ;;; of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2017, 2019, 2020 Inria
+;;; Copyright © 2017, 2019, 2020, 2021 Inria
 
 (define-module (ufrgs ufrgs)
   #:use-module (guix)
@@ -210,7 +210,7 @@ development platform for in-memory data.  It specifies a standardized
 language-independent columnar memory format for flat and hierarchical data,
 organized for efficient analytic operations on modern hardware. This package
 provides an interface to the 'Arrow C++' library.")
-   (license #f)))
+   (license license:asl2.0)))
 
 (define-public r-data-tree
 (package

commit 4caa636b1843704ce302611fc10518d4922bb74d

Fix license name.
Regression introduced in 45cd7aeb495f230fb3735cf74e14584144ae6d28.

* airbus/gcvb.scm (python-dash-bootstrap-components)[license]: Fix
name.

airbus/gcvb.scm
@@ -333,7 +333,7 @@
       "Bootstrap themed components for use in Plotly Dash")
     (description
       "Bootstrap themed components for use in Plotly Dash")
-    (license license:asl2)))
+    (license license:asl2.0)))
 
 (define-public python-pyyaml-5
   (package
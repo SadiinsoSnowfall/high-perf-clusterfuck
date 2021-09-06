77d1ef2a70b119c42e5d63f245e97931cc1e50d6
Marek Felšöci <marek.felsoci@inria.fr>
2021-09-06 10:55:13 +0200

    update hmat-oss

--- airbus/hmat-oss.scm
+++ airbus/hmat-oss.scm
@@ -1,12 +1,12 @@
 ;;; This module extends GNU Guix and is licensed under the same terms, those
 ;;; of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2017, 2019 Inria
+;;; Copyright © 2017, 2019, 2021 Inria
 
 (define-module (airbus hmat-oss)
   #:use-module (guix)
   #:use-module (guix git-download)
-  #:use-module (guix licenses)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system cmake)
   #:use-module (gnu packages)
   #:use-module (gnu packages gcc)
@@ -17,25 +17,29 @@
   #:use-module (srfi srfi-1))
 
 (define-public hmat-oss
-  (package
-   (name "hmat-oss")
-   (version "bf70719e963a858de52dade5defb49ad02f94248")
-   (home-page "https://github.com/jeromerobert/hmat-oss")
-   (synopsis "A hierarchical matrix C/C++ library including a LU solver.")
-   (description synopsis)
-   (license gpl2)
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url home-page)
-           (commit version)))
-     (sha256
-      (base32
-       "0353yf6jwmgynp2c31aq7qv8j95x26apn9dmzqcdllw60l38qh2h"))))
-   (build-system cmake-build-system)
-   (arguments
-    '(#:configure-flags '("-DBUILD_EXAMPLES=ON")))
-   (inputs
-    `(("jemalloc" ,jemalloc)
-      ("lapack" ,openblas)))))
+  (let
+      ((commit "6a4e21bb86cda66396f27d3473471c52a2357edc")
+       (revision "32"))
+    (package
+     (name "hmat-oss")
+     (version
+      (git-version "1.7.1" revision commit))
+     (home-page "https://github.com/jeromerobert/hmat-oss")
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url home-page)
+             (commit commit)))
+       (sha256
+        (base32
+         "1alkzahpw55bqy01yscvl7sl2r4mhyv7i1qs12wmzhapqsw36jvg"))))
+     (build-system cmake-build-system)
+     (arguments
+      '(#:configure-flags '("-DBUILD_EXAMPLES=ON")))
+     (inputs
+      `(("jemalloc" ,jemalloc)
+        ("lapack" ,openblas)))
+     (synopsis "A hierarchical matrix C/C++ library including an LU solver.")
+     (description #f)
+     (license license:gpl2))))
